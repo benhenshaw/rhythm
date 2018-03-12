b8f7d59c43fd9114d0760aeb4a82bc85040f916e
benhenshaw <benedicthenshaw@gmail.com>
2018-03-12 19:20:15 +0000

    Revamped sound converter

--- src/sound_converter.c
+++ src/sound_converter.c
@@ -4,77 +4,43 @@
 // This is a separate program used to convert .wav audio files to the custom
 // audio format. It is quick and dirty, so don't look too closely.
 //
+// Usage:
+//     sc input.wav output.sound
+//
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_stdinc.h>
 
-typedef float f32;
-typedef _Bool bool;
-#define true 1
-#define false 0
+#include "common.c"
 
 typedef struct {
     f32 * samples;
     int sample_count;
 } Sound;
 
-Sound read_pcm(char * file_name) {
-    Sound result = {};
-
-    FILE * file = fopen(file_name, "r");
-    if (!file_name) return result;
-
-    int samples_read = 0;
-    int total_samples_read = 0;
-    int chunk_sample_count = 512;
-
-    f32 * samples = malloc(chunk_sample_count);
-    if (!samples) return result;
-
-    while (true) {
-        samples_read = fread(samples, sizeof(f32), chunk_sample_count, file);
-        total_samples_read += samples_read;
-        if (samples_read > 0) {
-            samples = realloc(samples, total_samples_read + chunk_sample_count);
-            if (!samples) return result;
-        } else {
-            break;
-        }
-    }
-
-    result.samples = samples;
-    result.sample_count = total_samples_read;
-
-    return result;
-}
-
-bool write_pfm(Sound sound, char * file_name) {
+bool write_sound_file(Sound sound, char * file_name) {
     FILE * file = fopen(file_name, "w");
     if (!file_name) return false;
-    fprintf(file, "AUDIO_F32\nSAMPLE_COUNT %d\nENDHDR\n", sound.sample_count);
-    int samples_written = fwrite(sound.samples, sizeof(f32), sound.sample_count, file);
-    return samples_written == sound.sample_count;
+    fprintf(file, "SND\nSAMPLE_COUNT %d\nENDHDR\n", sound.sample_count);
+    int byte_count = sound.sample_count * sizeof(sound.samples[0]);
+    int bytes_written = fwrite(sound.samples, 1, byte_count, file);
+    fclose(file);
+    return byte_count == bytes_written;
 }
 
 int main(int argument_count, char ** arguments) {
-    if (argument_count < 2) {
-        fprintf(stderr, "Usage:\n\t%s in.pcm out.sound\n", arguments[0]);
-        return 0;
-    }
-
-    char * in_file_name  = arguments[1];
-    char * out_file_name = arguments[2];
-
-    Sound sound = read_pcm(in_file_name);
-    if (sound.samples) {
-        printf("Read %d samples from '%s'.\n", sound.sample_count, in_file_name);
-
-        if (write_pfm(sound, out_file_name)) {
-
-        } else {
-
-        }
-    } else {
-        perror("ERROR: File '%s' could not be read");
-    }
+    SDL_AudioSpec spec = { .format = AUDIO_F32, .freq = 48000, .channels = 1 };
+    u8 * stream;
+    u32 byte_count = 0;
+    printf("Loading '%s'.\n", arguments[1]);
+    SDL_assert(SDL_LoadWAV(arguments[1], &spec, &stream, &byte_count));
+    printf("%d bytes loaded.\n", byte_count);
+    Sound sound = {
+        .samples = (f32 *)stream,
+        .sample_count = byte_count / sizeof(f32),
+    };
+    printf("Writing %d samples (%.2fs) to '%s'.\n",
+        sound.sample_count, (f32)sound.sample_count / (f32)spec.freq, arguments[2]);
+    SDL_assert(write_sound_file(sound, arguments[2]));
+    printf("Success!\n");
 }
